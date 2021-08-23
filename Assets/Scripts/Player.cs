4c5ceec7b5710d53620618b102d6f2a5e44723d0
rymc88 <64868815+rymc88@users.noreply.github.com>
2021-08-23 15:10:42 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _jumpHeight;
+    private float _yVelocity;
+    private bool _canDoubleJump;
+    
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _playerSpeed;
+
+        if(_controller.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+            
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+                
+            }
+
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+}
